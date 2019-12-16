3e92393751de55a32b5282ed2a2a142c467f8b61
Sandra Tatarevićová <sandra.tatarevicova@ackee.cz>
2019-12-16 13:54:33 +0100

    🎨 Improve sqlproxy service account name

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "random_id" "instance_name_suffix" {
 
 locals {
   instance_name                 = "${var.project}-${var.environment}-${random_id.instance_name_suffix.hex}"
-  sqlproxy_service_account_name = "${var.project}-${var.environment}"
+  sqlproxy_service_account_name = "${var.project}-sqlproxy-${var.environment}"
   project_name_normalized       = replace(var.project, "-", "_")
   postgres_database_name        = local.project_name_normalized
   postgres_database_user        = local.postgres_database_name
